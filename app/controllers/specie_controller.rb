95fc75b10e36964dd64ccbfbcd2ad9ac16618027
Miguel Rust <hava12soft@gmail.com>
2018-07-06 22:28:12 -0600

    Added species index page.

--- app/controllers/specie_controller.rb
+++ app/controllers/specie_controller.rb
@@ -1,6 +1,9 @@
 class SpecieController < ActionController::Base
   protect_from_forgery with: :exception
   layout "application"
+  def index
+    @species = Specie.all.order("common ASC")
+  end
   def show
     @specie = Specie.find(params[:id])
     @locations = Location.joins(:encounters).where("encounters.specie_id = #{@specie.id}")
