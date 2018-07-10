d833d776b672a0839ad75cc5cc2f141602847424
Miguel Rust <hava12soft@gmail.com>
2018-07-09 21:58:11 -0600

    Implemented material design nav bar styles.

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -4,22 +4,14 @@ let profileMenu = document.querySelector(".menu-profile");
 let profileItems = document.querySelector(".menu-profile-items");
 let lastWidth = window.innerWidth;
 toggleButton.addEventListener("click",function(){
-  menu.style.display = (menu.style.display == "flex") ? "none" : "flex";
-  toggleButton.innerHTML = (menu.style.display == "none") ? "Menu" : "Close";
+  menu.classList.toggle("menu__hide");
+  menu.classList.toggle("menu__show");
+  toggleButton.classList.toggle("toggle__open");
+  toggleButton.classList.toggle("toggle__closed");
   if(menu.style.display == "none") profileItems.classList.remove("menu-profile-items-hover");
 });
 if(document.querySelectorAll(".menu-profile").length != 0){
   profileMenu.addEventListener("click",function(){
     if(window.innerWidth <= 500) profileItems.classList.toggle("menu-profile-items-hover");
   });
-}
-window.onresize = function(){
-  if(window.innerWidth >= 500){
-    menu.style.display = "flex";
-    toggleButton.innerHTML = "Menu";
-    if(document.querySelectorAll(".menu-profile").length != 0) profileItems.classList.remove("menu-profile-items-hover");
-  }else if(lastWidth >= 500){
-    menu.style.display = "none";
-  }
-  lastWidth = window.innerWidth;
 }
