db704c4714dc4b3126afc38989441917bf5acd5d
Miguel Rust <hava12soft@gmail.com>
2018-07-14 22:36:55 -0600

    Implemented back button helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
   def back_button props
-     link_to "<i class='fas fa-undo'></i>Back".html_safe, (props[:path] == nil) ? root_path : props.path, class: 'green-button'
+     link_to "<i class='fas fa-undo'></i>Back".html_safe, (props[:path] == nil) ? root_path : props[:path], class: 'green-button'
   end
 end
