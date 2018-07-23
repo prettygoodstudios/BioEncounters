e29f825ecfa28dbfeea29a04f10502aab824a4c9
Miguel Rust <hava12soft@gmail.com>
2018-07-23 11:16:09 -0600

    Made some updates to the locations controller for API version 1.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::LocationsController < ApiController
   before_action :set_location, only: [:show, :update]
   before_action :is_authenticated, only: [:create, :update]
   before_action :is_mine, only: [:update]
+
   def index
-    render json: Location.all
+    render json: Location.find_by_sql("SELECT l.*, COUNT(e.id) as pop FROM locations l LEFT JOIN encounters e ON e.location_id = l.id GROUP BY l.id").map { |l| {city: l.city, full_address: l.full_address, id: l.id, latitude: l.latitude, longitude: l.longitude, encounters: l.pop} }
   end
   def show
-    render json: @location
+    l = @location
+    render json: { city: l.city, full_address: l.full_address, id: l.id, latitude: l.latitude, longitude: l.longitude, encounters: l.encounters }
   end
   def create
     @location = Location.new(location_params)
