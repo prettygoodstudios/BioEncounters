a11d5704733fbc8af5fb134aab355428465ec27b
Miguel Rust <hava12soft@gmail.com>
2019-07-17 21:39:13 -0600

    Built basic mobile gestures into the header.

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -3,15 +3,35 @@ let menu = document.querySelector(".menu");
 let profileMenu = document.querySelector(".menu-profile");
 let profileItems = document.querySelector(".menu-profile-items");
 let lastWidth = window.innerWidth;
-toggleButton.addEventListener("click",function(){
+let touchStartX = 0;
+let touchStartY = 0;
+
+const toggleMenu = () => {
+  console.log("Toggling");
   menu.classList.toggle("menu__hide");
   menu.classList.toggle("menu__show");
   toggleButton.classList.toggle("toggle__open");
   toggleButton.classList.toggle("toggle__closed");
   if(menu.style.display == "none") profileItems.classList.remove("menu-profile-items-hover");
-});
+}
+
+toggleButton.addEventListener("click", toggleMenu);
 if(document.querySelectorAll(".menu-profile").length != 0){
   profileMenu.addEventListener("click",function(){
     if(window.innerWidth <= 500) profileItems.classList.toggle("menu-profile-items-hover");
   });
 }
+
+document.addEventListener("touchstart", ({touches}) => {
+  const {clientX, clientY} = touches[0];
+  touchStartX = clientX;
+  touchStartY = clientY;
+});
+
+document.addEventListener("touchend", ({changedTouches}) => {
+  const {clientX, clientY} = changedTouches[0];
+  const shouldToggle = (menu.className.indexOf("menu__show") == -1) ? (clientX - touchStartX > 50 && touchStartX < 100) : (touchStartX - clientX > 50 && touchStartX < document.querySelector(".menu__show").clientWidth);
+  if(shouldToggle) toggleMenu();
+  touchStartX = 0;
+  touchStartY = 0;
+});
