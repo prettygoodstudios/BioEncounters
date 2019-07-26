dbd232f8120bb59fb6087798a1d7a4088adab6db
Miguel Rust <hava12soft@gmail.com>
2019-07-25 21:01:42 -0600

    Fixed issue with identifying duplicate encounters.

--- app/controllers/encounter_controller.rb
+++ app/controllers/encounter_controller.rb
@@ -214,7 +214,7 @@ class EncounterController < ActionController::Base
         spec = Specie.where("common='#{e[:specie][:common_name]}'")
       end
       spec = spec.first.id
-      dupes = Encounter.all.to_a.rindex { |n| n.description == e[:observations] && n.specie_id = spec } != -1
+      dupes = Encounter.all.to_a.rindex { |n| n.description == e[:observations] && n.specie_id = spec }
       if dupes == nil
         loco.encounters.create!(description: e[:observations], date: e[:date], specie_id: spec)
         successes += 1
