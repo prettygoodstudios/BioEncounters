c8317a21f3de068057578ba07cd80264af89bc58
Miguel Rust <hava12soft@gmail.com>
2019-07-25 20:48:53 -0600

    Removed locations with zero encounters from location index view.

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -2,7 +2,7 @@ class LocationController < ActionController::Base
   protect_from_forgery with: :exception
   layout "application"
   def index
-    @locations = Location.order("ASCII(city)+ASCII(state)*10 ASC")
+    @locations = Location.find_by_sql("SELECT l.* FROM locations l LEFT JOIN encounters e ON e.location_id = l.id GROUP BY l.id HAVING COUNT(e.id) > 0 ORDER BY ASCII(city)+ASCII(state)*10 ASC")
   end
   def show
     @location = Location.find(params[:id])
@@ -13,7 +13,7 @@ class LocationController < ActionController::Base
 
   end
   def geo_json_api
-    render json: Location.find_by_sql("SELECT l.*, COUNT(e.id) as pop FROM locations l LEFT JOIN encounters e ON e.location_id = l.id GROUP BY l.id").map { |l| {city: l.city, full_address: l.full_address, id: l.id, latitude: l.latitude, longitude: l.longitude, encounters: l.pop} }
+    render json: Location.find_by_sql("SELECT l.*, COUNT(e.id) as pop FROM locations l LEFT JOIN encounters e ON e.location_id = l.id GROUP BY l.id HAVING COUNT(e.id) > 0").map { |l| {city: l.city, full_address: l.full_address, id: l.id, latitude: l.latitude, longitude: l.longitude, encounters: l.pop} }
   end
   def get_by_state
     @locations = Location.where("state = '#{params[:state]}'").order("city ASC")
